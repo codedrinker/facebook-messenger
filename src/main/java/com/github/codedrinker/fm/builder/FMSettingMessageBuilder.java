57050b6ea436e6415d25f02d7bc6408d9ef7e58d
codedrinker <gwangchunlei@gmail.com>
2017-01-31 20:41:36 +0800

    add setting message builder

---
+++ src/main/java/com/github/codedrinker/fm/builder/FMSettingMessageBuilder.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 Chunlei Wang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.codedrinker.fm.builder;
+
+import com.github.codedrinker.fm.entity.FMSettingMessage;
+
+import java.util.ArrayList;
+
+public class FMSettingMessageBuilder {
+
+    private FMSettingMessage fmSettingMessage;
+
+    public FMSettingMessageBuilder() {
+        this.fmSettingMessage = new FMSettingMessage();
+    }
+
+    public static FMSettingMessageBuilder defaultBuilder() {
+        return new FMSettingMessageBuilder();
+    }
+
+    public FMSettingMessageBuilder withGreeting(String text) {
+        FMSettingMessage.Greeting greeting = new FMSettingMessage.Greeting();
+        greeting.setText(text);
+        this.fmSettingMessage.setGreeting(greeting);
+        return this;
+    }
+
+    public FMSettingMessageBuilder withSettingType(FMSettingMessage.SettingType settingType) {
+        this.fmSettingMessage.setSetting_type(settingType);
+        return this;
+    }
+
+    public FMSettingMessageBuilder withThreadState(FMSettingMessage.ThreadState threadState) {
+        this.fmSettingMessage.setThread_state(threadState);
+        return this;
+    }
+
+    public FMSettingMessageBuilder withCallToAction(FMSettingMessage.CallToAction... callToActions) {
+        if (callToActions.length != 0) {
+            if (this.fmSettingMessage.getCall_to_actions() == null) {
+                this.fmSettingMessage.setCall_to_actions(new ArrayList<FMSettingMessage.CallToAction>());
+            }
+            for (FMSettingMessage.CallToAction callToAction : callToActions) {
+                this.fmSettingMessage.getCall_to_actions().add(callToAction);
+            }
+        }
+        return this;
+    }
+
+    public FMSettingMessage build() {
+        return this.fmSettingMessage;
+    }
+}
--- src/main/java/com/github/codedrinker/fm/entity/FMSettingMessage.java
+++ src/main/java/com/github/codedrinker/fm/entity/FMSettingMessage.java
@@ -23,7 +23,85 @@ public class FMSettingMessage {
     private ThreadState thread_state;
     private List<CallToAction> call_to_actions;
 
+    public SettingType getSetting_type() {
+        return setting_type;
+    }
+
+    public void setSetting_type(SettingType setting_type) {
+        this.setting_type = setting_type;
+    }
+
+    public Greeting getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(Greeting greeting) {
+        this.greeting = greeting;
+    }
+
+    public ThreadState getThread_state() {
+        return thread_state;
+    }
+
+    public void setThread_state(ThreadState thread_state) {
+        this.thread_state = thread_state;
+    }
+
+    public List<CallToAction> getCall_to_actions() {
+        return call_to_actions;
+    }
+
+    public void setCall_to_actions(List<CallToAction> call_to_actions) {
+        this.call_to_actions = call_to_actions;
+    }
+
     public static class CallToAction {
+
+        private String payload;
+        private FMEnum.WebViewHeightRatio webview_height_ratio;
+        private String url;
+        private String title;
+        private CallActionType type = CallActionType.postback;
+
+        public String getPayload() {
+            return payload;
+        }
+
+        public void setPayload(String payload) {
+            this.payload = payload;
+        }
+
+        public FMEnum.WebViewHeightRatio getWebview_height_ratio() {
+            return webview_height_ratio;
+        }
+
+        public void setWebview_height_ratio(FMEnum.WebViewHeightRatio webview_height_ratio) {
+            this.webview_height_ratio = webview_height_ratio;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public CallActionType getType() {
+            return type;
+        }
+
+        public void setType(CallActionType type) {
+            this.type = type;
+        }
 
         public CallToAction(String payload, FMEnum.WebViewHeightRatio webview_height_ratio, String url, String title) {
             this.payload = payload;
@@ -49,12 +127,6 @@ public class FMSettingMessage {
             this.title = title;
         }
 
-        private String payload;
-        private FMEnum.WebViewHeightRatio webview_height_ratio;
-        private String url;
-        private String title;
-        private CallActionType type = CallActionType.postback;
-
         public CallToAction() {
         }
 
@@ -65,6 +137,15 @@ public class FMSettingMessage {
 
     public static class Greeting {
         private String text;
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+
     }
 
     public enum SettingType {
